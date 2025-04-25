2e3fd63f8e7d32a1d2cae047ce91a0acbeb5d8c4
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-04-25 11:31:14 +0300

    Add Product Specification

---
+++ SportsShop.Core/Specifications/ISpecifications.cs
@@ -0,0 +1,30 @@
+﻿using SportsShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsShop.Core.Specifications
+{
+    public interface ISpecifications<T> 
+    {
+        public Expression<Func<T, bool>> Criteria { get; set; }
+
+        public List<Expression<Func<T, object>>> Includes { get; set; }
+
+        public Expression<Func<T, object>> OrderBy { get; set; }
+
+        public Expression<Func<T, object>> OrderByDesc { get; set; }
+
+        public bool IsDistinct { get; }
+
+        public int Skip { get; set; }
+
+        public int Take { get; set; }
+
+        public bool IsPaginationEnabled { get; set; }
+
+    }
+}
---
+++ SportsShop.Core/Specifications/Products/ProductSpecification.cs
@@ -0,0 +1,36 @@
+﻿using SportsShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsShop.Core.Specifications.Products
+{
+    public class ProductSpecification : BaseSpecification<Product>
+    {
+
+        public ProductSpecification(ProductSpecParams productSpecParams): base( x =>
+            (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search)) &&
+            (productSpecParams.Brands.Count == 0 || productSpecParams.Brands.Contains(x.Brand)) &&
+            (productSpecParams.Types.Count == 0 || productSpecParams.Types.Contains(x.Type))
+            )
+        {
+            ApplyPagination(productSpecParams.PageSize * (productSpecParams.PageIndex - 1), productSpecParams.PageSize);
+
+            switch (productSpecParams.Sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(x => x.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDesc(x => x.Price);
+                    break;
+                default:
+                    AddOrderBy(x => x.Name);
+                    break;
+            }
+        }
+
+    }
+}
